82f7495c2fdf44f36c28e131c3f5f5bbed7ba12e
ciawalsh <cia.walsh@gmail.com>
2015-04-10 16:16:04 +0100

    Calculator can now replace set '0' with entered digits

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,6 +12,19 @@ class ViewController: UIViewController
 {
 
     @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    
 }
 
