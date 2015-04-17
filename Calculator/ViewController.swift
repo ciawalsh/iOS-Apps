53ec635cf30d1d49439ca0c15a8c568e06fab73c
ciawalsh <cia.walsh@gmail.com>
2015-04-17 17:15:43 +0100

    Multiply working, but would like to refactor

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -35,13 +35,20 @@ class ViewController: UIViewController
 //            case "-":
 //            case "÷":
             
-            case "✕":
-                if operandStack.count >= 2 {
-                    displayValue = operandStack.removeLast() * operandStack.removeLast()
-                    enter()
-                }
+            case "✕": performOperation(multiply)
         default: break
         }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func multiply(op1: Double, op2: Double) -> Double {
+        return op1 * op2
     }
     
     var operandStack = Array<Double>()
